aecbee06a70ba2903fcd1830239a571f85e792da
Jack Parrigon <jackparrigon@gmail.com>
2025-06-23 13:12:04 -0700

    update main to draw player

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pygame
 from constants import *
+from player import *
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     running = True
     while running:
         for event in pygame.event.get():
@@ -13,7 +17,11 @@ def main():
              running = False
              return running
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.get_time()/1000
+
 
 if __name__ == "__main__":
     main()
